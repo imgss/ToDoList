ca8a6f6127f110aba8f9391503ac013889f604d3
imgss <838632526@qq.com>
2016-12-05 05:42:32 -0600

    Add files via upload
    实现标记完成，新增“todo项目”功能

---
+++ todolist/js/todo.js
@@ -0,0 +1,104 @@
+/**
+ * Created by gg on 2016/12/4.
+ */
+Vue.component('todoitem',{
+    data:function(){
+        return {
+            danger:['fa-thermometer-quarter','fa-thermometer-half','fa-thermometer-three-quarters','fa-thermometer-full']
+        }
+    },
+    props:['todo'],
+    template:'<div class="section">\
+                    <div>\
+                        <i class="fa fa-3x" :class="ji" title="紧急程度"></i>\
+                    </div>\
+                    <div>\
+                        <div>待完成：{{todo.value}}</div>\
+                        <div>完成状态：{{todo.isFinished?"已完成":"未完成"}}</div>\
+                        <div>截止时间：{{todo.deadline}}</div>\
+                        <div>备注：{{todo.tips}}</div>\
+                    </div>\
+                    <div>\
+                        <i class="fa fa-check-square fa-2x" title="完成" @click="finished"></i>\
+                        <i class="fa fa-times-circle fa-2x" title="删除"></i>\
+                    </div>\
+                </div>',
+    computed:{
+        ji:function(){
+            var em=this.danger[this.todo.isImportant],obj={};
+            obj[em]=true;
+            return obj;
+            }
+    },
+    methods:{
+        finished:function(){//完成按钮处理函数，点击完成后改变状态
+            console.log(showItem.todos);
+            this.todo.isFinished=true;
+        }
+    }
+
+});
+var addItem=new Vue({//增加一条项目
+    el:'#add',
+    data:{
+        show:false,
+        newToDo:{
+            isImportant:0,
+            isFinished:false,
+            value:'',
+            tips:'',
+            deadline:''
+        }
+    },
+    methods:{
+        copyObj:function(old){//向组件传递数据时，复制对象
+            if (Object.assign){
+                return Object.assign({},old);
+            }
+            var nObj={},prop;
+            for(prop in old){
+                if(old.hasOwnProperty(prop)){
+                    nObj[prop]=old[prop];
+                }
+            }
+            return nObj;
+        },
+        submit:function(){
+
+            this.newToDo.isImportant=parseInt(this.newToDo.isImportant);//接受到的isImportant是字符串，转换为数字
+            var copy=this.copyObj(this.newToDo);
+            //showItem.todos.push(this.newToDo);//这样只会传递引用
+            showItem.todos.push(copy);//showItem.todos[length]=copy不会触发视图更新？？
+
+        }
+    }
+});
+var showItem=new Vue({
+    el:'#todolist',
+    data:{
+        todos:[
+            {
+                isImportant:1,
+                isFinished:false,
+                value:'洗衣服',
+                tips:'不洗完不睡觉',
+                deadline:'14:00'
+            },
+            {
+                isImportant:2,
+                isFinished:false,
+                value:'做demo',
+                tips:'',
+                deadline:'16:00'
+            },
+            {
+                isImportant:3,
+                isFinished:false,
+                value:'去拍照',
+                tips:'',
+                deadline:'18:00'
+            }
+            
+        ]
+    }
+});
