11a4ad72e3b92773d55e9febd5a9b197e2b04c3e
imgss <838632526@qq.com>
2017-02-16 21:27:04 +0800

    加入简单markdown编辑器

--- src/components/todolist/write.vue
+++ src/components/todolist/write.vue
@@ -1,13 +1,46 @@
 <template>
- <div  class='todolist info'> under construction!</div>
+    <div class='todolist editor'>
+        <textarea v-model="md"></textarea>
+        <div v-html="html" id='board'>html</div>
+     </div>
 </template>
 
 <script>
+    import {
+        markdown
+    } from 'markdown'
+
+    export default {
+        data: function() {
+            return {
+                md: '# string'
+            }
+        },
+        computed: {
+            html: function() {
+                return markdown.toHTML(this.md);
+
+            }
+        }
+    }
 </script>
 
 <style scoped>
-    .info {
-        font-size: 3em;
+    .editor {
         color: #333;
+        margin-bottom: 20px;
+        width: 83%;
+        height: 70vh;
+    }
+    
+    textarea,
+    #board {
+        border: 1px solid #aaa;
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+        width: 49%;
+        height: 100%;
+        overflow: auto;
     }
 </style>
